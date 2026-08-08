2d6080d030ab4df5ea64e9c7f18d59c3b3bc9d0d
Hemanth <hemanthganneni@gmail.com>
2026-08-08 19:49:50 +0530

    feat(updater): discover trusted GitHub releases

---
+++ native/update/UpdateReleaseClient.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "update/UpdateManifest.h"
+
+#include <QByteArray>
+#include <QHash>
+#include <QObject>
+#include <QPointer>
+#include <QUrl>
+
+#include <functional>
+
+class QNetworkAccessManager;
+class QNetworkReply;
+
+namespace Colosseum::Update {
+
+struct ReleaseCheckResult {
+    enum class Status { Valid, NotModified, Rejected, NetworkError } status = Status::Rejected;
+    Manifest manifest;
+    QHash<QString, QUrl> assetUrls;
+    QString etag;
+    QString errorCode;
+};
+
+struct ReleaseClientConfig {
+    QUrl latestReleaseUrl;
+    QString repository = QStringLiteral("kingoftheseas56/Colosseum");
+    QByteArray publicKey;
+    bool allowHttpForTests = false;
+    int timeoutMs = 15000;
+};
+
+class UpdateReleaseClient final : public QObject {
+    Q_OBJECT
+public:
+    using Callback = std::function<void(ReleaseCheckResult)>;
+
+    UpdateReleaseClient(QNetworkAccessManager* nam, ReleaseClientConfig config,
+                        QObject* parent = nullptr);
+
+    void checkLatest(const QString& priorEtag, Callback done);
+    void cancel();
+
+private:
+    struct FetchResult {
+        bool transportOk = false;
+        bool bodyTooLarge = false;
+        bool unsafeRedirect = false;
+        int httpStatus = 0;
+        QByteArray body;
+        QString etag;
+        QString errorCode;
+    };
+
+    void fetch(const QUrl& url, qint64 cap, const QByteArray& priorEtag,
+               std::function<void(FetchResult)> done);
+    void finish(ReleaseCheckResult result);
+    bool allowedUrl(const QUrl& url) const;
+
+    QNetworkAccessManager* m_nam = nullptr;
+    ReleaseClientConfig m_config;
+    QPointer<QNetworkReply> m_reply;
+    Callback m_done;
+    bool m_cancelled = false;
+};
+
+} // namespace Colosseum::Update
